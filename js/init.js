42592a55f102f5b4a01ff13b0bf2e7e7936b4dc6
evaluacion43m <evaluacion43m@gmail.com>
2023-11-29 22:39:50 -0600

    actualizado

--- js/init.js
+++ js/init.js
@@ -2,12 +2,12 @@
 //Firebase 
 const firebaseConfig = {
     apiKey: "AIzaSyBQeAbpd04i8Z7FVIr96beX6wm-3HekZbY",
-    authDomain: "maquillaje-7e53c.firebaseapp.com",
-    projectId: "maquillaje-7e53c",
-    storageBucket: "maquillaje-7e53c.appspot.com",
-    messagingSenderId: "610004161858",
-    appId: "1:610004161858:web:9680fdaf4590e2d4abe4db",
-    measurementId: "G-7HQ97SYVKX"
+  authDomain: "maquillaje-7e53c.firebaseapp.com",
+  projectId: "maquillaje-7e53c",
+  storageBucket: "maquillaje-7e53c.appspot.com",
+  messagingSenderId: "610004161858",
+  appId: "1:610004161858:web:9680fdaf4590e2d4abe4db",
+  measurementId: "G-7HQ97SYVKX"
 };
 
 firebase.initializeApp(firebaseConfig);
